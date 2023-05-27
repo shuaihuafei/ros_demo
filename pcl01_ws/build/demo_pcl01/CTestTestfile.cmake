f309c8e88b455174cdeb0fc7263d9a59c138ed9f
shuaihuafei <1223391749@qq.com>
2023-05-27 20:52:50 +0800

    delete pcl01_ws

--- pcl01_ws/build/demo_pcl01/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shuai/pcl01_ws/src/demo_pcl01
-# Build directory: /home/shuai/pcl01_ws/build/demo_pcl01
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- pcl01_ws/build/test_hello/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shuai/pcl01_ws/src/test_hello
-# Build directory: /home/shuai/pcl01_ws/build/test_hello
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
